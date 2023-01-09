3c2dffbfc53a7dd823b63661c843c67e209c613c
lwysockiffw <lukasz.wysocki@pfizer.com>
2023-01-09 14:25:49 +0100

    Adding html reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,13 @@ const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
+//get today date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+today = dd + mm + yyyy;
+
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
@@ -19,6 +26,15 @@ async function setupNodeEvents(on, config) {
 }
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: '[status]_[datetime]-[name]-report',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: 'cypress/reports/' + today,
+  },
   e2e: {
     specPattern: '**/*.feature',
     supportFile: 'cypress/support/index.js',
