a895ea2ef217f7926b3073bf68a0807d02307232
lwysockiffw <lukasz.wysocki@pfizer.com>
2023-01-11 10:30:50 +0100

    adding locator strategy

---
+++ cypress/pageObject/locators/locators.js
@@ -0,0 +1,33 @@
+/*
+  Create JavaScript objects that will store the DOM elements locators.
+  Example:
+  const duckduckgoHomepage = {
+    searchBar: "#search_form_input_homepage",
+    searchBtn: "#search_button_homepage",
+  }
+*/
+
+/*
+  Initialized empty object that.
+  List every object name created above.
+  Example:
+  const locatorPages = { group1, group2, group3 }
+
+  Do not change the name of the object.
+  Uncomment line below and add group names in {}.
+*/
+// const locatorPages = {  }
+
+/*
+ Funtion that checks if the locator passed in step is not empty.
+ It retrieves the DOM element from Objects above.
+*/
+
+export function readLocator(element) {
+  locatorDivided = element.split('.')
+  let parent = locatorDivided[0]
+  let child = locatorDivided[1]
+  if (parent && child) {
+    return locatorPages[parent][child]
+  }
+}
